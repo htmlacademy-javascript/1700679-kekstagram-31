9d44b7388a14378c89963aced8e911ef82476c16
Van1s1mys <malkovsin@gmail.com>
2024-02-16 20:31:34 +0500

    tests

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,9 @@
 
 const isStringLength = (string, strLength) => string.length === strLength;
 
+console.log(isStringLength('проверяемая   строка', 20));
+console.log(isStringLength('проверяемая строка', 10));
+
 const isPallindrom = (string) => {
   string = string.replaceAll(' ', '').toLowerCase();
   let reverseString = '';
@@ -10,12 +13,25 @@ const isPallindrom = (string) => {
   return reverseString === string;
 };
 
+console.log(isPallindrom("топот"));
+console.log(isPallindrom("ДовОд"));
+console.log(isPallindrom('Лёша на полке клопа нашёл'));
+console.log(isPallindrom('Лёша на полке клопа нл'));
+
 const getNumbers = (num) => {
-  num = num.toString();
+  num = num.toString().replaceAll(' ', '').split('');
   let numStr = num.map((el) => isNaN(Number(el)) ? '' : el);
+  numStr = numStr.join('');
   if (!numStr.length) {
     numStr += 'NaN';
   }
 
   return numStr;
 };
+
+console.log(getNumbers('2023 год'));
+console.log(getNumbers('а я томат'));
+
+console.log(getNumbers(2023));
+console.log(getNumbers(-1));
+console.log(getNumbers(1.5));
