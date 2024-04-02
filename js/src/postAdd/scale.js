0ab59f87dffbdf1eb69cfd4d9eac02afea800563
Van1s1mys <malkovsin@gmail.com>
2024-04-02 09:23:35 +0500

    (scale)[script] refrectoring

--- js/src/postAdd/scale.js
+++ js/src/postAdd/scale.js
@@ -2,8 +2,23 @@ const uploadForm = document.querySelector('.img-upload__form');
 const photoEditorForm = uploadForm.querySelector('.img-upload__overlay');
 const scaleControl = photoEditorForm.querySelector('.scale__control--value');
 const previewImage = photoEditorForm.querySelector('.img-upload__preview img');
+const scaleControlSmaller = photoEditorForm.querySelector('.scale__control--smaller');
+const scaleControlBigger = photoEditorForm.querySelector('.scale__control--bigger');
 
 let scale = 100;
+
+const initScaleController = () => {
+  scaleControlSmaller.addEventListener('click', handleScaleControlSmallerClick);
+  scaleControlBigger.addEventListener('click', handleScaleControlBiggerClick);
+};
+
+const destroyScaleController = () => {
+  scale = 100;
+  previewImage.style.transform = `scale(${scale / 100})`;
+  scaleControl.value = `${scale}%`;
+  scaleControlSmaller.removeEventListener('click', handleScaleControlSmallerClick);
+  scaleControlBigger.removeEventListener('click', handleScaleControlBiggerClick);
+}
 
 function handleScaleControlSmallerClick() {
   scale -= 25;
@@ -23,4 +38,4 @@ function handleScaleControlBiggerClick() {
   previewImage.style.transform = `scale(${scale / 100})`;
 }
 
-export { handleScaleControlSmallerClick, handleScaleControlBiggerClick };
+export { initScaleController, destroyScaleController };
