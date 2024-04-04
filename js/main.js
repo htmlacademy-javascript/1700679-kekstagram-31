e3e29f4ef1ddefbb78daef2553f6c5c23a552389
Van1s1mys <malkovsin@gmail.com>
2024-04-04 22:29:01 +0500

    remove prerender

--- js/main.js
+++ js/main.js
@@ -2,11 +2,9 @@ import {uploadImage} from './src/postAdd/addPostForm';
 import {getData} from './src/api/api';
 import {timedMessage} from './src/api/messages';
 import initThumbnailsSorting from './src/postRender/thumbnailsSorting';
-import {createThumbnails} from './src/postRender/thumbnails';
 const ERROR_DELAY = 5000;
 
 getData().then((posts) => {
-  createThumbnails(posts);
   initThumbnailsSorting(posts);
 // eslint-disable-next-line no-unused-vars
 }).catch((err) => {
